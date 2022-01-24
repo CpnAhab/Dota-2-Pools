d7aaa4f332dc9ee523f86bf97cbf9ddabba1d216
CpnAhab <77088271+CpnAhab@users.noreply.github.com>
2022-01-23 22:54:12 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,72 @@
+import os
+from flask import Flask, flash, render_template, request, session, redirect
+import random
+
+str = [
+    'abaddon', 'alchemist', 'axe', 'beastmaster', 'brewmaster', 'bristleback', 'centaur', 'chaos_knight', 'rattletrap', 'dawnbreaker', 'doom_bringer', 'dragon_knight', 'earth_spirit', 'earthshaker',
+    'elder_titan', 'huskar', 'wisp', 'kunkka', 'legion_commander', 'life_stealer', 'lycan', 'magnataur', 'marci', 'mars', 'night_stalker', 'omniknight', 'phoenix',
+    'pudge', 'sand_king', 'slardar', 'snapfire', 'spirit_breaker', 'sven', 'tidehunter', 'shredder', 'tiny', 'treant', 'tusk', 'abyssal_underlord', 'undying', 'skeleton_king']
+
+agi = [
+    'antimage', 'arc_warden', 'bloodseeker', 'bounty_hunter', 'broodmother', 'clinkz', 'drow_ranger', 'ember_spirit', 'faceless_void', 'gyrocopter', 'hoodwink', 
+    'juggernaut', 'lone_druid', 'luna', 'medusa', 'meepo', 'mirana', 'monkey_king', 'morphling', 'naga_siren', 'nyx_assassin', 'pangolier', 'phantom_assassin', 'phantom_lancer',
+    'razor', 'riki', 'nevermore', 'slark', 'sniper', 'spectre', 'templar_assassin', 'terrorblade', 'troll_warlord', 'ursa', 'vengefulspirit', 'venomancer', 'viper', 'weaver']
+
+int = [
+    'ancient_apparition', 'bane', 'batrider', 'chen', 'crystal_maiden', 'dark_seer', 'dark_willow', 'dazzle', 'death_prophet', 'disruptor', 'enchantress', 'enigma', 'grimstroke', 'invoker',
+    'jakiro', 'keeper_of_the_light', 'leshrac', 'lich', 'lina', 'lion', 'furion', 'necrolyte', 'ogre_magi', 'oracle', 'obsidian_destroyer', 'puck', 'pugna', 'queenofpain', 'rubick',
+    'shadow_demon', 'shadow_shaman', 'silencer', 'skywrath_mage', 'storm_spirit', 'techies', 'tinker', 'visage', 'void_spirit', 'warlock', 'windrunner', 'winter_wyvern', 'witch_doctor', 'zuus']
+
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/request", methods=["POST"])
+def as_text():
+
+    pool = list()
+    for hero in str:
+        if request.form.get(hero):
+            pool.append(hero)
+    for hero in agi:
+        if request.form.get(hero):
+            pool.append(hero)
+    for hero in int:
+        if request.form.get(hero):
+            pool.append(hero)
+
+    if request.form["op"] == "random":
+        if not pool:
+            hero = "No heroes to choose from."
+        else:
+            hero = f"{random.choice(pool)} chosen from {len(pool)} heroes."
+        
+        return render_template("index.html", hero=hero)
+
+    elif request.form["op"] == "to_text":
+        if not pool:
+            pool = "No heroes selected."
+        else:
+            pool = ", ".join(pool)
+
+        return render_template("index.html", pool=pool)
+
+    elif request.form["op"] == "download":
+        return redirect("/")
+
+    elif request.form["op"] == "to_sb":
+        return redirect("/")
+
