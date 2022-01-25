70d4b4fa4ca9293fef1cbabdd157f6b6a001aaaf
Jake Haversat <jake.haversat@gmail.com>
2022-01-25 02:57:26 -0500

    fixin this sum

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ def translate_names(pool: list) -> str:
     query = f"SELECT externalname FROM heroes WHERE internalname IN {pool}"
 
     conn, curs = connect(DB_URL)
+    console.log(results)
     results = ", ".join(curs.execute(query))
     close(conn, curs)
 
